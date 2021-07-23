05a03c6ebe6eb488fba5921786824e8a97092a85
gu-nogueira <gus.h.nogueira@gmail.com>
2021-07-22 23:22:52 -0300

    Sono..

---
+++ desafio03/src/app/models/Deliverymen.js
@@ -0,0 +1,19 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Deliverymen extends Model {
+  static init(sequelize) {
+    super.init({
+      name: Sequelize.STRING,
+      path: Sequelize.STRING,
+      url: {
+        type: Sequelize.VIRTUAL,
+      }
+    },
+    {
+      sequelize,
+    });
+    return this;
+  };
+}
+
+export default Deliverymen;
